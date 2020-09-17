a8240fb5496bc0fbbca9c174ab1e0833c410652d
Semyon <iboyarnikoff@gmail.com>
2020-09-18 04:36:14 +0500

    fix: немного поправил вёрстку анкеты заполнения нового счёта

--- src/components/common/Modals/CompanyAccounts/CompanyAccounts.tsx
+++ src/components/common/Modals/CompanyAccounts/CompanyAccounts.tsx
@@ -47,34 +47,37 @@ export function CompanyAccounts({
       <Modal.Body>
         <Form className="form bank-guarantee-form">
           <FormGroup className="form-group bank-guarantee-form-type">
-            <ControlLabel className="form-label">
-              Наименование банка
-            </ControlLabel>
-            <Input
-              type="text"
-              placeholder="ПАО Абсолют-банк"
-              required
-              onChange={getFieldUpdater('bankName')}
-            />
-            <ControlLabel className="form-label">Номер счета</ControlLabel>
-            <Input
-              type="text"
-              placeholder="3453 1243 3621 1211"
-              required
-              onChange={getFieldUpdater('accountNumber')}
-            />
-            <ControlLabel className="form-label">БИК</ControlLabel>
-            <Input
-              type="text"
-              placeholder="0292341542113"
-              required
-              onChange={getFieldUpdater('bik')}
-            />
-            <Button
-              className="bank-guarantee-form-submit"
-              skin="inverse"
-              onClick={handleSumbit}
-            >
+            <div className="form-field-row">
+              <ControlLabel className="form-label">
+                Наименование банка
+              </ControlLabel>
+              <Input
+                type="text"
+                placeholder="ПАО Абсолют-банк"
+                required
+                onChange={getFieldUpdater('bankName')}
+              />
+            </div>
+            <div className="form-field-row">
+              <ControlLabel className="form-label">Номер счета</ControlLabel>
+              <Input
+                type="text"
+                placeholder="3453 1243 3621 1211"
+                required
+                onChange={getFieldUpdater('accountNumber')}
+              />
+            </div>
+            <div className="form-field-row">
+              <ControlLabel className="form-label">БИК</ControlLabel>
+              <Input
+                type="text"
+                placeholder="0292341542113"
+                required
+                onChange={getFieldUpdater('bik')}
+              />
+            </div>
+
+            <Button skin="inverse" onClick={handleSumbit}>
               Добавить
             </Button>
           </FormGroup>
