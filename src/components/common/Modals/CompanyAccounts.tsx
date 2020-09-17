d382b30917db95fd57963a52e16c57d23c22d518
Semyon <iboyarnikoff@gmail.com>
2020-09-17 11:37:41 +0500

    wip: отправка данных на бэк

--- src/components/common/Modals/CompanyAccounts.tsx
+++ src/components/common/Modals/CompanyAccounts.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { Modal, Form, FormGroup, ControlLabel, Input } from 'rsuite';
+import { b4Transport, TCompanyAccount } from '../../../transport';
 
 import { Button } from '../Button';
 
@@ -13,6 +14,22 @@ export function CompanyAccounts({
   show,
   toggle
 }: TGuaranteeModalProps): JSX.Element {
+  const [state, setState] = React.useState<Partial<TCompanyAccount>>({
+    bankName: '',
+    accountNumber: '',
+    bik: ''
+  });
+
+  function getFieldUpdater(
+    field: keyof TCompanyAccount
+  ): (value: string) => void {
+    return (value: string): void => setState({ ...state, [field]: value });
+  }
+
+  function handleSumbit() {
+    b4Transport.setCompanyAccount(state);
+  }
+
   return (
     <Modal
       dialogClassName="modal guarantee-modal"
@@ -29,12 +46,31 @@ export function CompanyAccounts({
             <ControlLabel className="form-label">
               Наименование банка
             </ControlLabel>
-            <Input type="text" placeholder="ПАО Абсолют-банк" />
+            <Input
+              type="text"
+              placeholder="ПАО Абсолют-банк"
+              required
+              onChange={getFieldUpdater('bankName')}
+            />
             <ControlLabel className="form-label">Номер счета</ControlLabel>
-            <Input type="text" placeholder="3453 1243 3621 1211" />
+            <Input
+              type="text"
+              placeholder="3453 1243 3621 1211"
+              required
+              onChange={getFieldUpdater('accountNumber')}
+            />
             <ControlLabel className="form-label">БИК</ControlLabel>
-            <Input type="text" placeholder="0292341542113" />
-            <Button className="bank-guarantee-form-submit" skin="inverse">
+            <Input
+              type="text"
+              placeholder="0292341542113"
+              required
+              onChange={getFieldUpdater('bik')}
+            />
+            <Button
+              className="bank-guarantee-form-submit"
+              skin="inverse"
+              onClick={handleSumbit}
+            >
               Добавить
             </Button>
           </FormGroup>
