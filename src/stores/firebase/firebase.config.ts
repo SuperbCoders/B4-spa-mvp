65f97ed4c8e6a9e2c0ba0a89595451aa6ac4d623
Semyon <iboyarnikoff@gmail.com>
2020-09-17 17:05:06 +0500

    fix: поправил конфиг для firebase

--- src/stores/firebase/firebase.config.ts
+++ src/stores/firebase/firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAK3uAlNUJFZs7c9tzK3AW9gZ631L_IVSM',
-  authDomain: 'ultra-ace-274620.firebaseapp.com',
-  databaseURL: 'https://ultra-ace-274620.firebaseio.com',
-  projectId: 'ultra-ace-274620',
-  storageBucket: 'ultra-ace-274620.appspot.com',
-  messagingSenderId: '833229266892',
-  appId: '1:833229266892:web:9800715c1e65e5bf394769'
+  apiKey: 'AIzaSyCKj_Xm_U5TIEHQyWlTRkGWuM4hKHEfo_I',
+  authDomain: 'b4-mvp.firebaseapp.com',
+  databaseURL: 'https://b4-mvp.firebaseio.com',
+  projectId: 'b4-mvp',
+  storageBucket: 'b4-mvp.appspot.com',
+  messagingSenderId: '951888432148',
+  appId: '1:951888432148:web:04ac6b0b505b408bb1eb01'
 };
