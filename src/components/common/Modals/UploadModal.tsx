17c44363a7e87378c03800114139ee04785f43a1
Semyon <iboyarnikoff@gmail.com>
2020-09-18 17:42:53 +0500

    feat: добавлено состояние экрана, когда файлы были загружены

--- src/components/common/Modals/UploadModal.tsx
+++ src/components/common/Modals/UploadModal.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 import { Modal } from 'rsuite';
+import { userCompanyDataSended } from '../../../stores';
+import { Button } from '../Button';
 import { Dropzone } from '../Dropzone';
 
 type TUploadMpdalProps = {
@@ -9,6 +11,14 @@ type TUploadMpdalProps = {
 };
 
 export function UploadModal({ show, toggle }: TUploadMpdalProps): JSX.Element {
+  const [isUploaded, setIsUploaded] = React.useState(false);
+
+  React.useEffect((): VoidFunction => {
+    const sub = userCompanyDataSended.documentsSended$.subscribe(setIsUploaded);
+
+    return (): void => sub.unsubscribe();
+  }, []);
+
   return (
     <Modal
       dialogClassName="modal upload-modal"
@@ -18,39 +28,57 @@ export function UploadModal({ show, toggle }: TUploadMpdalProps): JSX.Element {
       onHide={toggle}
       backdrop={true}
     >
-      <Modal.Header>Приложить сканы документов</Modal.Header>
+      <Modal.Header>{!isUploaded && `Приложить сканы документов`}</Modal.Header>
       <Modal.Body>
-        <div className="upload-modal-row">
-          <div className="upload-modal-dropzone-col">
-            <div className="mvp-dropzone">
-              <Dropzone />
+        {!isUploaded && (
+          <div className="upload-modal-row">
+            <div className="upload-modal-dropzone-col">
+              <div className="mvp-dropzone">
+                <Dropzone />
+              </div>
             </div>
-          </div>
-          <div className="upload-modal-disclaimer-col">
-            <div className="documents-disclaimer">
-              <h3 className="documents-disclaimer-title">
-                Список документов, которые необходимо приложить
-              </h3>
+            <div className="upload-modal-disclaimer-col">
+              <div className="documents-disclaimer">
+                <h3 className="documents-disclaimer-title">
+                  Список документов, которые необходимо приложить
+                </h3>
 
-              <ul className="documents-disclaimer-list">
-                <li className="documents-disclaimer-list-item">
-                  Бухгалтерская отчетность (год)
-                </li>
-                <li className="documents-disclaimer-list-item">
-                  Бухгалтерская отчетность (квартал)
-                </li>
-                <li className="documents-disclaimer-list-item">Устав</li>
-                <li className="documents-disclaimer-list-item">
-                  Решение/Доверенность
-                </li>
-                <li className="documents-disclaimer-list-item">Паспорт</li>
-                <li className="documents-disclaimer-list-item">
-                  Договор Аренды /право Cобстветнности
-                </li>
-              </ul>
+                <ul className="documents-disclaimer-list">
+                  <li className="documents-disclaimer-list-item">
+                    Бухгалтерская отчетность (год)
+                  </li>
+                  <li className="documents-disclaimer-list-item">
+                    Бухгалтерская отчетность (квартал)
+                  </li>
+                  <li className="documents-disclaimer-list-item">Устав</li>
+                  <li className="documents-disclaimer-list-item">
+                    Решение/Доверенность
+                  </li>
+                  <li className="documents-disclaimer-list-item">Паспорт</li>
+                  <li className="documents-disclaimer-list-item">
+                    Договор Аренды /право Cобстветнности
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+        {isUploaded && (
+          <div className="info-modal-results">
+            <p>
+              Cпасибо! 
+              <br />
+              Мы обрабатываем документы
+            </p>
+            <Button
+              skin="light"
+              className="info-modal-results-button"
+              onClick={toggle}
+            >
+              Вернуться на главную
+            </Button>
+          </div>
+        )}
       </Modal.Body>
     </Modal>
   );
--- src/components/common/Modals/index.ts
+++ src/components/common/Modals/index.ts
@@ -1,7 +1,6 @@
 import './style.scss';
 
 export { UploadModal } from './UploadModal';
-export { InfoModal } from './InfoModal';
 export { GuaranteeModal } from './GuaranteeModal';
 export { LoginModal } from './LoginModal';
 export { CompanyAccounts } from './CompanyAccounts';
