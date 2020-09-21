071b481204f7171e3c460dd95bb963c3eb7776b8
Semyon <iboyarnikoff@gmail.com>
2020-09-21 13:01:04 +0500

    feat:  добавил русскую локализацию компонентам

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import { IntlProvider } from 'rsuite';
 
 import { ErrorBoundary } from './ErrorBoundary';
 import { Landing, MVP01, MVP02, MVP03 } from './components/pages';
@@ -11,27 +12,31 @@ import { LoginModal } from './components/common/Modals';
 import { ModalsStore } from './stores';
 import { routerHistory } from './router-history';
 
+import ruRU from 'rsuite/lib/IntlProvider/locales/ru_RU';
+
 function AppComponent(): JSX.Element {
   return (
-    <ErrorBoundary>
-      <Router history={routerHistory}>
-        <Switch>
-          <Route
-            exact
-            path={`/company/:${COMPANY_INN_ROUTE_KEY}`}
-            component={Landing}
+    <IntlProvider locale={ruRU}>
+      <ErrorBoundary>
+        <Router history={routerHistory}>
+          <Switch>
+            <Route
+              exact
+              path={`/company/:${COMPANY_INN_ROUTE_KEY}`}
+              component={Landing}
+            />
+            <Route path="/dashboard/01" component={MVP01} />
+            <Route path="/dashboard/02" component={MVP02} />
+            <Route path="/dashboard/03" component={MVP03} />
+            <Redirect from="/" to="/dashboard/02" />
+          </Switch>
+          <LoginModal
+            show={ModalsStore.instance.isLoginModalOpened}
+            toggle={ModalsStore.instance.toggleLoginModal}
           />
-          <Route path="/dashboard/01" component={MVP01} />
-          <Route path="/dashboard/02" component={MVP02} />
-          <Route path="/dashboard/03" component={MVP03} />
-          <Redirect from="/" to="/dashboard/02" />
-        </Switch>
-        <LoginModal
-          show={ModalsStore.instance.isLoginModalOpened}
-          toggle={ModalsStore.instance.toggleLoginModal}
-        />
-      </Router>
-    </ErrorBoundary>
+        </Router>
+      </ErrorBoundary>
+    </IntlProvider>
   );
 }
 
