603d72cd9f55d8e9c12bf3b2b208ff574d4f7009
Semyon <iboyarnikoff@gmail.com>
2020-10-03 23:17:39 +0500

    feat: скрывать кнопку заявки до момента, пока будут обработаны доки

--- src/components/pages/MainPage/components/WarriantyClaimFormOpenButton/WarriantyClaimFormOpenButton.tsx
+++ src/components/pages/MainPage/components/WarriantyClaimFormOpenButton/WarriantyClaimFormOpenButton.tsx
@@ -4,21 +4,43 @@ import { ModalsOpenerService } from '../../modals-opener.service';
 import { ReactComponent as Plus } from '../../../../../assets/images/svg/plus.svg';
 
 import './style.scss';
+import { currentCompanyStorage } from '../../../../../stores';
+import { TCompanyLandingInfo } from '../../../../../transport';
 
-export function WarriantyClaimFormOpenButton(): JSX.Element {
-  return (
-    <Whisper
-      placement="topEnd"
-      trigger="hover"
-      speaker={<Tooltip className="mvp-tooltip">Заявка на гарантию</Tooltip>}
-    >
-      <IconButton
-        onClick={ModalsOpenerService.openWarrantyModal}
-        className="guarantee-add-button"
-        skin="primary"
-        circle
-        icon={<Plus width="24" height="24" />}
-      />
-    </Whisper>
-  );
-}
+export const WarriantyClaimFormOpenButton = React.memo(
+  (): JSX.Element => {
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    React.useEffect(
+      (): VoidFunction => {
+        const sub = currentCompanyStorage.currentCompany$.subscribe(
+          (currentCompany: TCompanyLandingInfo | null): void => {
+            setIsVisible(Boolean(currentCompany?.wasProcessed));
+          }
+        );
+
+        return (): void => sub.unsubscribe();
+      }
+    );
+
+    if (!isVisible) return <></>;
+
+    return (
+      <Whisper
+        placement="topEnd"
+        trigger="hover"
+        speaker={<Tooltip className="mvp-tooltip">Заявка на гарантию</Tooltip>}
+      >
+        <IconButton
+          onClick={ModalsOpenerService.openWarrantyModal}
+          className="guarantee-add-button"
+          skin="primary"
+          circle
+          icon={<Plus width="24" height="24" />}
+        />
+      </Whisper>
+    );
+  }
+);
+
+WarriantyClaimFormOpenButton.displayName = 'WarriantyClaimFormOpenButton';
