b3efbd2fc7f26f657288fbafed2497748b92b0b6
Semyon <iboyarnikoff@gmail.com>
2020-10-06 02:44:40 +0500

    fix: не throttle, а debounce нужен в потоках зависимостей

--- src/components/pages/CabinetPage/components/consts.ts
+++ src/components/pages/CabinetPage/components/consts.ts
@@ -3,7 +3,7 @@ import {
   userCompanyDataSended,
   currentCompanyStorage
 } from '../../../../stores';
-import { throttleTime } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
 
 const DEBOUNCE_TIME = 300;
 
@@ -11,14 +11,14 @@ export const processNotificationCardVisibilityStreams$ = combineLatest([
   userCompanyDataSended.companyAccountsSended$,
   userCompanyDataSended.documentsSended$,
   currentCompanyStorage.currentCompany$
-]).pipe(throttleTime(DEBOUNCE_TIME));
+]).pipe(debounceTime(DEBOUNCE_TIME));
 
 export const uploadCompanyAccountsActionCardVisibilityStreams$ = combineLatest([
   currentCompanyStorage.currentCompany$,
   userCompanyDataSended.companyAccountsSended$
-]).pipe(throttleTime(DEBOUNCE_TIME));
+]).pipe(debounceTime(DEBOUNCE_TIME));
 
 export const uploadDocumentsActionCardVisibilityStreams = combineLatest([
   currentCompanyStorage.currentCompany$,
   userCompanyDataSended.documentsSended$
-]).pipe(throttleTime(DEBOUNCE_TIME));
+]).pipe(debounceTime(DEBOUNCE_TIME));
