7a2062c3ce288bc86b516df10499384f657c802b
Semyon <iboyarnikoff@gmail.com>
2020-10-06 16:34:41 +0500

    refactor: заиспользовал useRxStream в обёртке, проверяющей авторизацию

--- src/WithAuth.tsx
+++ src/WithAuth.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 import { firebaseStore } from './stores';
 import { Auth } from './components/pages';
+import { useRxStream } from './utils/hooks';
 
 export function WithAuth(Component: React.ElementType): () => JSX.Element {
   return function WrappedComponent(): JSX.Element {
-    const [isLoggedIn, setLoggedIn] = React.useState(false);
-
-    React.useEffect((): VoidFunction => {
-      const sub = firebaseStore.isLoggedIn$.subscribe(setLoggedIn);
-
-      return (): void => sub.unsubscribe();
-    }, []);
+    const isLoggedIn = useRxStream(firebaseStore.isLoggedIn$, false);
 
     return isLoggedIn ? <Component /> : <Auth />;
   };
